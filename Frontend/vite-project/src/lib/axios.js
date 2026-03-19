520346a30640061f48ec075acfb4fda3c17f38dc
Avinash Kumar <avinashkumarmgr24@gmail.com>
2026-03-19 18:13:25 +0530

    Update axios.js

--- Frontend/vite-project/src/lib/axios.js
+++ Frontend/vite-project/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chatify-backend-4qij.onrender.com",
+  baseURL: "https://chatify-backend-4qij.onrender.com/api",
   withCredentials: true,
 });

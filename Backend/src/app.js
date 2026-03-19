09c717e86b6d49551efbbf4e21032098fbb69e80
Avinash Kumar <avinashkumarmgr24@gmail.com>
2026-03-19 09:52:59 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,6 @@ const corsOptions = {
 };
 // cors updated
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
